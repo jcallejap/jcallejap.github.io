d0970fdef179ecda8279ef6fd2715ab9c850d0b7
Jesús Calleja <chuscp@gmail.com>
2023-11-26 20:01:51 +0100

    Add a test Pelican theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 AUTHOR = 'jcallejap'
 SITENAME = 'Programando en tiempo real'
 SITEURL = ''
@@ -5,10 +7,25 @@ SITEURL = ''
 PATH = 'content'
 
 TIMEZONE = 'Europe/Rome'
-
 DEFAULT_LANG = 'es'
 
 OUTPUT_PATH = 'output/'
+
+# Theme related
+THEME = 'Flex'
+FAVICON = SITEURL + "/images/favicon.ico"
+I18N_TEMPLATES_LANG = "es"
+DEFAULT_LANG = "es"
+OG_LOCALE = "es_ES"
+LOCALE = "es_ES"
+
+# Main Menu
+MAIN_MENU = True
+MENUITEMS = (
+    ("Archives", "/archives"),
+    ("Categories", "/categories"),
+    ("Tags", "/tags"),
+)
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -24,7 +41,8 @@ LINKS = (('Pelican', 'https://getpelican.com/'),
 # Social widget
 SOCIAL = (('StackOverflow', 'https://stackoverflow.com/users/218774/j-calleja'),)
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 4
+COPYRIGHT_YEAR = datetime.now().year
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
